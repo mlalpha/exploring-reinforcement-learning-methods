bf31ba4f4f31e222e3142508057502d17c5c7641
edward1991704 <edward1997104@link.cuhk.edu.hk>
2018-11-11 10:41:23 +0800

    1. added the predicted reward function

--- transition-genration/k_step_planning_agent.py
+++ transition-genration/k_step_planning_agent.py
@@ -30,7 +30,7 @@ class KStepPlanningAgent:
             actions = [self.env.action_space.sample() for _ in range(branch)]
             
         action_index = np.where(rewards == np.max(rewards))[0][0]
-        return first_actions[action_index]
+        return first_actions[action_index], np.max(rewards) / self.k_step
         
         
 
@@ -46,27 +46,31 @@ if __name__ == '__main__':
     num_of_episodes = 20000
 
     scores_deque = deque(maxlen=100)
+    predicted_score_deque = deque(maxlen=100)
     scores = []
     max_score = -np.Inf
     for i_episode in range(1, num_of_episodes+1):
         state = env.reset()
         score = 0
+        predicted_score = 0
         for t in range(max_step):
             # env.render()
-            action = agent.step(state)
+            action, predicted_reward = agent.step(state)
             # action = env.action_space.sample()
             next_state, reward, done, _ = env.step(action)
             state = next_state
             score += reward
+            predicted_score += predicted_reward
             if done:
                 break 
         scores_deque.append(score)
+        predicted_score_deque.append(predicted_score)
         scores.append(score)
         if score > max_score:
             max_score = score
         # print('Episode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
         if i_episode % 10 == 0:
-            print('Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
+            print('Episode {}\tAverage Score: {:.2f}\t predicted Score: {:.2f}'.format(i_episode, np.mean(scores_deque), np.mean(predicted_score_deque)))   
             print('Max Score: {:.2f}\n'.format(max_score))
 
     print('Avg Score: {:.2f}\n'.format(np.mean(scores)))
